aa5395bd95d7c2c282178d17ca47866188f86ba0
Ryan Kelley <rskelley9@gmail.com>
2014-06-20 19:15:10 -0400

    cleaned up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 10.times do
 
   Patient.create(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name, email:Faker::Internet.email, password: Faker::Internet.password)
