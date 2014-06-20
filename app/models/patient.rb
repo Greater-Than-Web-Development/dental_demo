98215a0396f5ea1bfc69c6ee339c1c7e911ffb0f
Ryan Kelley <rskelley9@gmail.com>
2014-06-20 06:24:50 -0400

    associations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,6 @@
 class Patient < ActiveRecord::Base
   attr_accessible :date_of_birth, :email, :first_name, :last_name, :phone
+
+  has_many :appointments
+  has_many :appointment_requests
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140620015923) do
+ActiveRecord::Schema.define(:version => 20140620084409) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -45,5 +45,45 @@ ActiveRecord::Schema.define(:version => 20140620015923) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "appointment_requests", :force => true do |t|
+    t.integer  "patient_id"
+    t.date     "date"
+    t.time     "time"
+    t.boolean  "morning?"
+    t.string   "appointment_type"
+    t.boolean  "request_read?"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "appointments", :force => true do |t|
+    t.integer  "patient_id"
+    t.integer  "chair_id"
+    t.date     "date"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.string   "appointment_type"
+    t.boolean  "confirmation_sent?"
+    t.boolean  "confirmed?"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "chairs", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "patients", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone"
+    t.date     "date_of_birth"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
 end
