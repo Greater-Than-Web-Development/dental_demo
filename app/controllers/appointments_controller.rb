28c42f86152db13b3293def69b26b176338c2585
Ryan Kelley <rskelley9@gmail.com>
2014-06-27 19:06:02 -0400

    .

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,11 +2,11 @@ class AppointmentsController < ApplicationController
 
   def new
     if patient_signed_in?
-      @patient = Patient.find_by_id(current_patient.id)
-      @appointment_request = AppointmentRequest.new
-      patient_session[:id] = current_patient.id
-      patient_session[:email] = current_patient.email
-      patient_session[:first_name] = current_patient.first_name
+      # @patient = Patient.find_by_id(current_patient.id)
+      # @appointment_request = AppointmentRequest.new
+      # patient_session[:id] = current_patient.id
+      # patient_session[:email] = current_patient.email
+      # patient_session[:first_name] = current_patient.first_name
       render 'new', layout: "appointments"
     else
       redirect_to new_patient_session_path
@@ -15,9 +15,9 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    p "These are my params: #{params[:afternoon]}"
-    p patient_session
-    p patient_session[:email]
+    # p "These are my params: #{params[:afternoon]}"
+    # p patient_session
+    # p patient_session[:email]
     if current_patient.check_date_of_birth(params[:date_of_birth]) && current_patient.check_email(params[:email])
       AppointmentRequest.new(patient_id: current_patient.id, date: params[:date], appointment_type: params[:appointment_type])
       render :success
