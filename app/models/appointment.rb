ca65dfcc14b67b5430bea0398aa1def544d33aad
Ryan Kelley <rskelley9@gmail.com>
2014-07-06 23:42:40 -0400

    WIP: appointment scheduling conflicts feature

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -30,21 +30,52 @@ class Appointment < ActiveRecord::Base
     TimeDifference.between(self.start_time, self.end_time).in_hours
   end
 
-  def all_available_times(time_start="9:00am")
-    time = Time.parse(time_start)
+  def all_available_times(time_input="9:00am")
+    time_min = Time.parse(time_input)
+    max_time = Time.parse("5:00pm")
     available_times = Array.new
-    hours = 0
-    16.times do
-      hours + 0.5
-      available_times << time + hours
+    hours = 0.5
+
+    while time_min <= max_time do
+      available_times << time_min
+      time_min += hours.hours
     end
+
     return available_times
   end
 
+  #generates all appointment blocks inside of appointment
+  def appointment_range(start_time=self.start_time, end_time=self.end_time)
+    step = 60*30 #1/2 hour
+    app_range = Array.new
+    while start_time <= end_time
+      app_range << start_time
+      start_time += step
+    end
+    return app_range
+  end
+
+
+  # Todo Create time slot manager class with these methods
+  # note ActiveRecord 3.2.11 does not support pluck with multiple attributes
+
+  # def self.available_times_for(given_date=Date.today)
+  #   appointment_slots = all_available_times()
+  #   # appointments = where(date: given_date).pluck(:start_time, :end_time)
+  #   appointments = where(date: given_date).select([:start_time, :end_time]).map {|a| {start_time: a.start_time, end_time: a.end_time}}
+  #   appointments.map do |appointment|
+  #     if appointment[0] || appointment[1]
+  #   end
+  # end
+
   def self.available_times_for(given_date=Date.today)
-    appointment_slots = all_available_times()
-    appointments = where(date: given_date).pluck(:start_time, :end_time)
-    ava
+    # if any available time is between scheduled start time and end time, remove from available array
+   available_times = all_available_times()
+   scheduled_times = where(date: given_date).select([:start_time, :end_time]).map {|a| {start_time: a.start_time, end_time: a.end_time}}
+   open_slots = Array.new
+   available_times.each do |time_slot|
+    if time_slot.between?()
+    end
   end
 
   def self.list_all_on(input_date)
@@ -77,29 +108,17 @@ end
   end
 
 
-# find appointments scheduled closest to this time
+#  def overlaps?(other_appointment)
+#   (self.start_time - other_appointment.end_time) * (other_appointment.start_time - self.end_time) >= 0
+# end
 
- #  def self.find_closted_appointment(requested_time, date)
+# def start_time_conflict?(start_time_string, end_time_string)
+#   self.start_time.between?(Time.zone.parse(start_time_string), Time.zone.parse(end_time_string))
+# end
 
- #   list_all_on(date).order(ABS(:start_time - requested_time)).first
-
- # end
-
- # Check if a given appointment overlaps this appointment
-
- def overlaps?(other_appointment)
-  (self.start_time - other_appointment.end_time) * (other_appointment.start_time - self.end_time) >= 0
-end
-
-def start_time_conflict?(start_time_string, end_time_string)
-  self.start_time.between?(Time.zone.parse(start_time_string), Time.zone.parse(end_time_string))
-end
-
-def end_time_conflict?(start_time_string, end_time_string)
-  self.end_time.between?(Time.zone.parse(end_time_string), Time.zone.parse(end_time_string))
-end
-
-
+# def end_time_conflict?(start_time_string, end_time_string)
+#   self.end_time.between?(Time.zone.parse(end_time_string), Time.zone.parse(end_time_string))
+# end
 
 # # Return a scope for all appointment overlapping the given appointment, including the given appointment itself
 # named_scope :overlapping, lambda { |appointment| {:conditions => ["(DATEDIFF(start_time, ?) * DATEDIFF(?, end_time)) >= 0", appointment.end_time, appointment.start_time]}}
