c5a7860d512121ffa94372e46a64fe0243817e48
Ryan Kelley <rskelley9@gmail.com>
2014-09-01 21:38:51 -0400

    WIP:seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["room one", "room two"].each do |room|
+  Chair.find_or_create_by_room room
+end
+
+["room one", "room two"].each do |room|
+  Chair.find_or_create_by_room room
+end
+
+AdminUser.create! do |a|
+  a.email = ENV[ADMIN_EMAIL]
+  a.password = ENV[ADMIN_PASSWORD] = 'password'
+end
+
+Patient.create! do |p|
+  p.email = ENV[ADMIN_EMAIL]
+  p.password = ENV[ADMIN_PASSWORD] = 'password'
+end
