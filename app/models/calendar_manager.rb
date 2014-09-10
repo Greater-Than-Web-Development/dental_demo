52d879b5a53c713ff4e07ad499484b60dd014d66
Ryan Kelley <rskelley9@gmail.com>
2014-09-10 04:16:25 -0400

    Calendar Manager finished time slot generator and add and remove methods

--- app/models/calendar_manager.rb
+++ app/models/calendar_manager.rb
@@ -18,46 +18,37 @@ class CalendarManager
     false
   end
 
+  #ToDo make method more flexible
   def generate_time_slots!
     # Work day begins at 9am
+    day = self.workday.date
     hour = 9
     minute = 0
     @time_slots = Array.new
 
-    33.times do
-      time = TimeOfDay.parse(hour) + minute.minutes
-      @time_slots << time.strftime("%I:%M %p")
-      minute += 15
-    end
-  end
-
-  def generate_time_slots!
-    # Work day begins at 9am
-    hour = 9
-    minute = 0
-    @time_slots = Array.new
-
-    #generate morning slots
     33.times do
       beginning = TimeOfDay.parse(hour) + minute.minutes
-      ending = start_time + 15.minutes
-      time_slot = TimeSlot.new work_day_id: self.workday.id, start_time: beginning.strftime("%I:%M %p"), end_time: ending.strftime("%I:%M %p")
+      ending = beginning + 15.minutes
+      time_slot = TimeSlot.create work_day_id: self.workday.id, start_time: beginning.strftime("%I:%M %p"), end_time: ending.strftime("%I:%M %p"), date: day
       @time_slots << time_slot
       minute += 15
     end
   end
 
-  array1.zip(array2).each do |array1_var, array2_var|
+  def add_new_slot(num_minutes= 60)
+    if @time_slots.empty?
+      last_slot = "09:00 AM"
+    else
+      last_slot = Time.parse(@time_slots.last)
+      new_slot = last_slot + num_minutes.minutes
 
-
-
-    slots.each { |key, value| original[key] = value.to_i }
-
-    ["room one", "room two"].each do |start_time|
-      Chair.find_or_create_by(room: room) do |chair|
-        chair.booked = [true,false][rand(2)]
-      end
+      @time_slots << new_slot.strftime("%I:%M %p")
     end
-
-
   end
+
+  def remove_slot(index=-1)
+    slot = @time_slots.slice!(index)
+    puts "Removed #{slot} from #{self.workday.date}"
+  end
+
+end
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,22 +2,22 @@ class TimeSlot < ActiveRecord::Base
 
   # each appointment has many time_slots and each time_slot appears in many appointments
 
-  validate :availability, unless: "appointments.nil?"
+  # validate :availability, unless: "appointments.nil?"
 
   has_many :bookings
   has_many :appointments, through: :bookings
   belongs_to :work_day
 
-  def availability
-    if self.chairs.count == 0
-      self.booked = false
-    elsif self.chairs.count == 2
-      self.booked = true
-    elsif self.chairs.count == 1 and self.appointment.type == "cleaning"
-      self.booked = false
-    else
-      self.booked = true
-    end
-  end
+  # def availability
+  #   if self.chairs.count == 0
+  #     self.booked = false
+  #   elsif self.chairs.count == 2
+  #     self.booked = true
+  #   elsif self.chairs.count == 1 and self.appointment.type == "cleaning"
+  #     self.booked = false
+  #   else
+  #     self.booked = true
+  #   end
+  # end
 
 end
